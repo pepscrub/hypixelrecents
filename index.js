7f349b0d91584e5879f0a4578d9b654d6c18ff7f
Brendan Freeman <brendanfreeman76@gmail.com>
2021-09-16 22:27:26 +1000

    Updated port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const mongo = require('mongodb');
 const middlewares = require('./api/middlewares');
 const path = require('path');
-const port = 8080;
+const port = 5000;
 
 module.exports.config = {
     APIKEY: "f2b0c24f-916f-47be-bd62-fbab213699ae",
